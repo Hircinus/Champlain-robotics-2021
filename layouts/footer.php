92a7eecb81d4149b2877f2378a113503b596ca1b
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-11-10 20:38:43 -0500

    updated for repl

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,7 +1,44 @@
-        <footer class="footer">
-            <div class="container">
-                <p>Hola</p>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-10 col-md-6 col-lg-3">
+                    <h5>The club</h5>
+                    <hr>
+                    <ul class="vertical">
+                        <li><a href="about">About us</a></li>
+                        <li><a href="about">Our team</a></li>
+                        <li><a href="about">About Champlain</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-10 col-md-6 col-lg-3">
+                    <h5>The CRC</h5>
+                    <hr>
+                    <ul class="vertical">
+                        <li><a href="about">Our robot</a></li>
+                        <li><a href="about">The game</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-10 col-md-6 col-lg-3">
+                    <h5>We're also on...</h5>
+                    <hr>
+                    <ul class="vertical">
+                        <li><a href="about">Facebook</a></li>
+                        <li><a href="about">Instagram</a></li>
+                        <li><a href="about">Snapchat</a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-10 col-md-4 col-lg-3">
+                    <h5>Techie stuff</h5>
+                    <hr>
+                    <ul class="vertical">
+                        <li><a href="about">About us</a></li>
+                        <li><a href="about">About us</a></li>
+                        <li><a href="about">About us</a></li>
+                        <li><a href="about">About us</a></li>
+                    </ul>
+                </div>
             </div>
-        </footer>
+        </div>
+    </footer>
     </body>
 </html>
